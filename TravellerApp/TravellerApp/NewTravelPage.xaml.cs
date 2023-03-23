d46bf0cc7df0de234b8b151f0e736e3d8498dbec
LovroProgramer <lovro.krapic@gmail.com>
2023-03-23 11:07:23 +0100

    Experience inserted

--- TravellerApp/TravellerApp/NewTravelPage.xaml.cs
+++ TravellerApp/TravellerApp/NewTravelPage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TravellerApp.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,9 +18,32 @@ namespace TravellerApp
             InitializeComponent();
         }
 
-        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
         {
+            Post post = new Post()
+            {
+                Experience = ExperienceEntry.Text
 
+            };
+            SQLiteConnection conn = new SQLiteConnection(App.DataBaseLocation);
+            conn.CreateTable<Post>();
+            var rows = conn.Insert(post);
+            conn.Close();
+
+            if (rows > 0)
+            {
+                await DisplayAlert("Success", "Experience succesfully inserted!", "OK");
+
+            }
+          
+    }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            SQLiteConnection conn = new SQLiteConnection(App.DataBaseLocation);
+            conn.CreateTable<Post>();
+            var posts = conn.Table<Post>().ToList();
+            conn.Close();
         }
     }
 }
---
+++ TravellerApp/TravellerApp/PostDetailPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravellerApp.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravellerApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PostDetailPage : ContentPage
+	{
+		Post SelectedPost;
+		public PostDetailPage (Post selectedPost)
+		{
+			InitializeComponent ();
+			this.SelectedPost = selectedPost;
+			ExperienceEntry.Text = selectedPost.Experience;
+		}
+
+        private async void DeleteButton_Clicked(object sender, EventArgs e)
+        {
+			using(SQLiteConnection conn = new SQLiteConnection(App.DataBaseLocation))
+			{
+				conn.CreateTable<Post>();
+				int rows = conn.Delete(SelectedPost);
+				if (rows > 0)
+				{
+					await DisplayAlert("Success", "Experience successfully deleted", "OK");
+				}
+				await Navigation.PopAsync();
+			}
+        }
+    }
+}
