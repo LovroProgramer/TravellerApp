9071dab9695c9d0d5b0e757ad0fbfeffac8fdc83
korisnik <korisnik@ODJ-O366-136>
2023-03-23 09:32:47 +0100

    Add project files.

---
+++ TravellerApp/TravellerApp/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravellerApp
+{
+    public partial class App : Application
+    {
+        public static string DataBaseLocation = string.Empty;
+        
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+        public App(string databaseLocation)
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+
+            DataBaseLocation = databaseLocation;
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
